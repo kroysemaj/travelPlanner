78d4e200f18a16de72f085a4c809f838e5439f58
James York <james@grandcircus.co>
2018-12-21 09:44:01 -0500

    Add logic for randomly selecting from the list

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const hills = require("./hills.json");
+const arctic = require("./monstersByTerrain/arctic.json");
+const coast = require("./monstersByTerrain/coasts.json");
+const desert = require("./monstersByTerrain/deserts.json");
+const forest = require("./monstersByTerrain/forests.json");
+const grassland = require("./monstersByTerrain/grasslands.json");
+const hill = require("./monstersByTerrain/hills.json");
+const mountain = require("./monstersByTerrain/mountains.json");
+const swamp = require("./monstersByTerrain/swamps.json");
+const underdark = require("./monstersByTerrain/underdark.json");
+const underwater = require("./monstersByTerrain/underwater.json");
+const urban = require("./monstersByTerrain/urban.json");
 
-console.log(hills.length);
+const terrainLists = {
+  arctic,
+  coast,
+  desert,
+  forest,
+  grassland,
+  hill,
+  mountain,
+  swamp,
+  underdark,
+  underwater,
+  urban
+};
+
+const terrain = process.env.terrain;
+const list = terrainLists[terrain];
+const rando = Math.floor(Math.random() * list.length);
+console.log(list[rando]);
